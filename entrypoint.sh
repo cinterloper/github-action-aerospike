fc1c5d348ad9ab445625c094849b10c2e999c4c0
Grant Haywood <grant@iowntheinter.net>
2022-12-07 10:39:56 -0700

    force mem 2gb

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,10 +16,10 @@ fi
 
 if [ -n "$3" ]; then
   config_file=$(basename $3)
-  docker_cmd="docker run -d --name gha_aerospike -p $1:3000 -p 3001:3001 -p 3002:3002 -p 3003:3003 \
+  docker_cmd="docker run -d --name gha_aerospike -e MEM_GB=2 -p $1:3000 -p 3001:3001 -p 3002:3002 -p 3003:3003 \
   $mount $image --config-file /opt/aerospike/etc/$config_file "
 else
-  docker_cmd="docker run -d --name gha_aerospike -p $1:3000 -p 3001:3001 -p 3002:3002 -p 3003:3003 $image"
+  docker_cmd="docker run -d --name gha_aerospike -p $1:3000 -e MEM_GB=2 -p 3001:3001 -p 3002:3002 -p 3003:3003 $image"
 fi
 
 echo $docker_cmd
