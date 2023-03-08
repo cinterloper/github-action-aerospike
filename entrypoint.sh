cbbe007cf4f052d8a2263d44e534670bd0ea2285
Grant Haywood <grant@iowntheinter.net>
2023-03-08 03:38:11 -0700

    count

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,7 +18,7 @@ echo "our /home"
 find /home/
 
 echo "host /home"
-ENTRY_COUNT=$(docker run -v "$FIREFLY_PATH":"$FIREFLY_PATH" -e FIREFLY_PATH -e AEROSPIKE_FETURES_B64 ubuntu:22.04 bash -x -c 'ls $FIREFLY_PATH')
+ENTRY_COUNT=$(docker run -v "$FIREFLY_PATH":"$FIREFLY_PATH" -e FIREFLY_PATH -e AEROSPIKE_FETURES_B64 ubuntu:22.04 bash -x -c 'ls $FIREFLY_PATH | wc -l')
 
 if [ $ENTRY_COUNT -eq 0 ]; then
   docker run -e CUR_DIR=$(pwd) -v$(pwd):$(pwd) -v "$FIREFLY_PATH":"$FIREFLY_PATH" -e FIREFLY_PATH -e AEROSPIKE_FETURES_B64 ubuntu:22.04 bash -x -c "cp -a $CUR_DIR/* $FIREFLY_PATH/"
