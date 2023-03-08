541d41f3ebeedb26b3eb28a7579a56cdd2ba5d24
Grant Haywood <grant@iowntheinter.net>
2023-03-08 02:12:39 -0700

    rewrite for debugging and clairity

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,34 +1,46 @@
 #!/bin/sh
 set -x
 set -e
+AEROSPIKE_PORT="$1"
+AEROSPIKE_VERSION="$2"
+AEROSPIKE_CONF="$3"
+AEROSPIKE_FETURES_B64="$4"
+FIREFLY_PATH="$5"
 
-mount=""
-if [ -n "$3" ] || [ -n "$4" ]; then
-  config_dir="$GITHUB_WORKSPACE/$(dirname $3)"
-  mount="-v $config_dir:/opt/aerospike/etc"
-  echo "list config dir" ls $config_dir
-fi
+env
+pwd
+ls
 
-if [ -n "$4" ]; then
-  feature_key_string=$(basename $4)
-  echo "$feature_key_string" | base64 -d > /github/workspace/.github/aerospike/features.conf
-  image="-e \"FEATURE_KEY_FILE=/opt/aerospike/etc/features.conf\" aerospike/aerospike-server-enterprise:$2"
-else
-  image="aerospike/aerospike-server:$2"
-fi
 
-if [ -n "$3" ]; then
-  config_file=$(basename $3)
-  docker_cmd="docker run -d --name gha_aerospike -e MEM_GB=2 -p $1:3000 -p 3001:3001 -p 3002:3002 -p 3003:3003 -p4333:4333 \
-  $mount $image --config-file /opt/aerospike/etc/$config_file "
-else
-  docker_cmd="docker run -d --name gha_aerospike -p $1:3000 -e MEM_GB=2 -p 3001:3001 -p 3002:3002 -p 3003:3003 -p4333:4333 $image"
-fi
+ls /
+
+ls /home/
+
+ls /opt
+
+
+mount="-v $FIREFLY_PATH/.github/aerospike:/opt/aerospike/etc"
+
+docker run -v "$FIREFLY_PATH":"$FIREFLY_PATH" -e FIREFLY_PATH -e AEROSPIKE_FETURES_B64 ubuntu:22.04 bash -x -c 'echo $AEROSPIKE_FETURES_B64 | base64 -d > $FIREFLY_PATH/.github/aerospike/features.conf'
+
+echo "$feature_key_string" | base64 -d > /github/workspace/.github/aerospike/features.conf
+
 
 echo $docker_cmd
-echo "will list /opt/aerospike/"
-docker run $mount $image find /opt/aerospike/
-ctr_id=$($docker_cmd)
-echo will sleep 10 seconds and check $ctr_id
+echo "will list /opt/aerospike/ with same mounts"
+docker run -v $FIREFLY_PATH/.github/aerospike:/opt/aerospike/etc ubuntu:22.04 bash -x -c 'ls /opt; find /opt/aerospike/'
+docker run -d --name gha_aerospike --rm \
+  -e MEM_GB=2 \
+  -e FEATURE_KEY_FILE=/opt/aerospike/etc/features.conf
+  -p $AEROSPIKE_PORT:3000 \
+  -p 3001:3001 \
+  -p 3002:3002 \
+  -p 3003:3003 \
+  -p 4333:4333 \
+  -v $FIREFLY_PATH/.github/aerospike:/opt/aerospike/etc \
+   aerospike/aerospike-server-enterprise:$AEROSPIKE_VERSION --config-file /opt/aerospike/etc/aerospike.conf
+
+
+echo will sleep 10 seconds and check gha_aerospike container
 sleep 10
-docker logs $ctr_id
+docker logs gha_aerospike
