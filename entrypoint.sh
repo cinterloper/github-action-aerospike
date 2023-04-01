9cf6a3ba8fd5a47a08ba400faba9974f0f8f99ed
Grant Haywood <grant@iowntheinter.net>
2023-03-31 19:29:17 -0600

    relative path

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,13 +7,13 @@ export AEROSPIKE_CONF="$3"
 export AEROSPIKE_FETURES_B64="$4"
 export FIREFLY_PATH="$5"
 
-echo $AEROSPIKE_FETURES_B64 | base64 -d >$FIREFLY_PATH/.github/aerospike/features.conf
+echo $AEROSPIKE_FETURES_B64 | base64 -d > .github/aerospike/features.conf
 
 virtualenv -p "$(which python3)" /tmp/venv
 source /tmp/venv/bin/activate
-pip3 install -r $FIREFLY_PATH/.github/aerospike/requirements.txt
+pip3 install -r .github/aerospike/requirements.txt
 
-python3 $FIREFLY_PATH/.github/aerospike/start_cluster.py \
+python3 .github/aerospike/start_cluster.py \
   --repo_path="$FIREFLY_PATH" \
   --aerospike_version="$AEROSPIKE_VERSION" \
   --features_file="$FIREFLY_PATH/.github/aerospike/features.conf"
