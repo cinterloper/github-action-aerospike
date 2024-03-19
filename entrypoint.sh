b91ffc46d793dadc19cdea74de5664ca51e9cb2e
Grant Haywood <grant@iowntheinter.net>
2024-03-18 19:15:42 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 set -x
 set -e
-export AEROSPIKE_PORT="$1"
-export AEROSPIKE_VERSION="$2"
+export AEROSPIKE_VERSION="$1"
+export AEROSPIKE_FETURES_B64="$2"
 export AEROSPIKE_CONF_TEMPLATE_B64="$3"
-export AEROSPIKE_FETURES_B64="$4"
-export FIREFLY_PATH="$5"
+
 
 echo $AEROSPIKE_FETURES_B64 | base64 -d >$FIREFLY_PATH/.github/aerospike/features.conf
 
@@ -22,5 +21,5 @@ fi
 python3 $FIREFLY_PATH/.github/aerospike/start_cluster.py \
   --repo_path="$FIREFLY_PATH" \
   --aerospike_version="$AEROSPIKE_VERSION" \
-  --features_file="$FIREFLY_PATH/.github/aerospike/features.conf" $CONF_PARAM
+  --features_file="$FIREFLY_PATH/.github/aerospike/features.conf" ${CONF_PARAM:-""}
   
