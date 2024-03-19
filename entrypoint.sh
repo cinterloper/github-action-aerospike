0e6e4ecfd266c4ff9313f4fb9ed3c4710103fba2
Grant Haywood <grant@iowntheinter.net>
2024-03-18 19:27:41 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,22 +4,23 @@ set -e
 export AEROSPIKE_VERSION="$1"
 export AEROSPIKE_FETURES_B64="$2"
 export AEROSPIKE_CONF_TEMPLATE_B64="$3"
+export REPOSITORY_ROOT="$4"
 
 
-echo $AEROSPIKE_FETURES_B64 | base64 -d >$FIREFLY_PATH/.github/aerospike/features.conf
+echo $AEROSPIKE_FETURES_B64 | base64 -d >$REPOSITORY_ROOT/.github/aerospike/features.conf
 
 virtualenv -p "$(which python3)" /tmp/venv
 source /tmp/venv/bin/activate
-pip3 install -r $FIREFLY_PATH/.github/aerospike/requirements.txt
+pip3 install -r $REPOSITORY_ROOT/.github/aerospike/requirements.txt
 
 if [[ ! -z "$AEROSPIKE_CONF_TEMPLATE_B64" ]]
 then
-  echo $AEROSPIKE_CONF_TEMPLATE_B64 | base64 -d > $FIREFLY_PATH/.github/aerospike/custom_template.conf
+  echo $AEROSPIKE_CONF_TEMPLATE_B64 | base64 -d > $REPOSITORY_ROOT/.github/aerospike/custom_template.conf
   CONF_PARAM="--config_template=custom_template.conf"
 fi
 
-python3 $FIREFLY_PATH/.github/aerospike/start_cluster.py \
-  --repo_path="$FIREFLY_PATH" \
+python3 $REPOSITORY_ROOT/.github/aerospike/start_cluster.py \
+  --repo_path="$REPOSITORY_ROOT" \
   --aerospike_version="$AEROSPIKE_VERSION" \
-  --features_file="$FIREFLY_PATH/.github/aerospike/features.conf" ${CONF_PARAM:-""}
+  --features_file="$REPOSITORY_ROOT/.github/aerospike/features.conf" ${CONF_PARAM:-""}
   
